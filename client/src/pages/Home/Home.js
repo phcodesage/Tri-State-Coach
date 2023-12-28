ae054e7110ebf257667167d418b1496d1337c22e
phcodesage <rechceltoledo@gmail.com>
2023-12-28 18:59:02 +0800

    added: pages

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css';
+function Home() {
+  return (
+    <div>
+        <div className='section_mobile'>
+            <h1>Home</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
