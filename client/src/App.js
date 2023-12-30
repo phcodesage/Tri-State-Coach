e4909d46d8d1699848f46c0584b67ddca7f37f8d
phcodesage <rechceltoledo@gmail.com>
2023-12-30 22:27:58 +0800

    Remove node_modules from tracking and update .gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.js';
+import CollegeShuttles from './pages/CollegeShuttles/CollegeShuttles.js';
+import GetAQuote from './pages/GetAQuote/GetAQuote.js';
+import ContactUs from './pages/ContactUs/ContactUs.js';
+import AboutUs from './pages/AboutUs/AboutUs.js'
+import Chartering from './pages/Chartering/Chartering.js';
+import Invoice from './pages/Invoice/Invoice.js'
+import Services from './pages/Services/Services.js'
+import TermsOfService from './pages/TermsOfService/TermsOfService.js'
+import PrivacyPolicy from './pages/Privacy-Policy/PrivacyPolicy.js'
 
 function App() {
-  useEffect(() => {
+  /*useEffect(() => {
     fetch('http://localhost:5000/api/tickets')
       .then(response => response.json())
       .then(data => setTickets(data))
       .catch(error => console.error(error));
-  }, []);
+  }, []);*/
   
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/about-us" component={AboutUs} />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/college-shuttles" element={<CollegeShuttles />} />
+        <Route path="/quote" element={<GetAQuote />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/charter-work" element={<Chartering />} />
+        <Route path="/invoice" element={<Invoice />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+      </Routes>
     </Router>
   );
 }
--- client/src/pages/Our-Services/OurServices.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function OurServices() {
-  return (
-    <div>OurServices</div>
-  )
-}
-
-export default OurServices
