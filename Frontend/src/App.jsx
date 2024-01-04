8ccdf2577a7b68a6f38788cdb5178efdb9bd8cfe
phcodesage <rechceltoledo@gmail.com>
2024-01-04 20:06:37 +0800

    added: tailwind

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,12 +11,57 @@ import Invoice from './Pages/Invoice/Invoice';
 import Services from './Pages/Services/Services';
 import TermsOfService from './Pages/TermsOfService/TermsOfService';
 import PrivacyPolicy from './Pages/Privacy-Policy/PrivacyPolicy';
-import Nav from './components/Nav/Nav';
 import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
+import styled from 'styled-components';
+
 
 function App() {
+  const HamburgerMenu = styled.div`
+  position: fixed;
+  top: 85px;
+  right: 65px;
+  z-index: 1000;
+  cursor: pointer;
+
+  svg {
+    fill: #DC2141;
+    width: 41px;
+    height: auto;
+  }
+
+  @media (max-width: 768px) {
+    top: 10px;
+    right: 10px;
+  }
+`;
+
+  
+const MenuOverlay = styled.div`
+z-index: 910;
+width: 613px;
+height: 100vh;
+float: right;
+grid-column-gap: 3vh;
+grid-row-gap: 3vh;
+border-left: 0px solid var(--dark-slate-grey);
+background-color: var(--dark-slate-grey);
+text-align: left;
+mix-blend-mode: normal;
+border-radius: 0;
+flex-direction: column;
+padding-left: 42px;
+padding-right: 69px;
+display: none;
+position: fixed;
+top: 0%;
+bottom: 0%;
+left: auto;
+right: 0%;
+box-shadow: inset 13px 0 18px 1px rgba(0,0,0,.18);
+`;
   const HamburgerIcon = () => (
     <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+
       <path d="M0 12H18V9.99984H0V12ZM0 6.99984H18V5.00016H0V6.99984ZM0 0V2.00016H18V0H0Z" fill="#DC2141" />
     </svg>
   );
@@ -26,11 +71,65 @@ function App() {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
+    
     <Router>
-      <div className="hamburger-menu" onClick={toggleMenu}>
+      <HamburgerMenu onClick={toggleMenu}>
         <HamburgerIcon />
-      </div>
-      {menuOpen && <Nav toggleMenu={toggleMenu} />}
+      </HamburgerMenu>
+
+      {menuOpen && (
+        <MenuOverlay className="menu_overlay">
+<div data-w-id="0a31589d-2c6a-068e-4473-e1383b9d3dc4" className="exit_menu">
+</div><div className="link_section_parent">
+<div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="book_a_ride_text">
+BOOK A RIDE</div></div><div className="nav_link">
+<a href="/" aria-current="page" className="link_block_text w-inline-block w--current">
+<div className="nav_link_text">
+Home<br /></div></a></div><div className="nav_link">
+<a href="/college-shuttles" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+College Shuttles<br /></div></a></div><div className="nav_link">
+<a href="/quote" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Get A Quote</div></a></div><div className="nav_link">
+<a href="/contact-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Contact Us</div></a></div></div><div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="about_us_text">
+ABOUT&nbsp;US</div></div><div className="nav_link">
+<a href="/about-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Our Buses<br /></div></a></div><div className="nav_link">
+<a href="/charter-work" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Chartering</div></a></div><div className="nav_link">
+<a href="/product/invoice" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Pay Invoice</div></a></div><div className="nav_link">
+<a href="/services" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Services</div></a></div><div className="nav_link secondary">
+<a href="/terms-of-service" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Terms of Service</div></a></div><div className="nav_link secondary">
+<a href="/privacy-policy" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Privacy Policy</div></a></div></div></div><div className="menu_action_icons">
+<a href="tel:+16315432500" className="link-block w-inline-block">
+<div className="menu_phone">
+</div></a><a href="#" className="link-block w-inline-block">
+<div className="menu_chat">
+</div></a><a href="mailto:regencybuses@gmail.com" className="link-block w-inline-block">
+<div className="menu_mail">
+</div></a><a href="https://goo.gl/maps/AGVfBWNNL3ZGRmVJ9" className="link-block w-inline-block">
+<div className="menu__location">
+</div></a></div></MenuOverlay>
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/college-shuttles" element={<CollegeShuttles />} />
--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 
+
 const StyledNav = styled.div`
 background-color: var(--tri-state-blue);
 height: 100vh;
@@ -27,48 +28,58 @@ const SpecialMessageText = styled.div`
   padding: 5px 0;
 `;
 
+const LogoImage = styled.img`
+
+
+  // Adjust size for smaller screens
+  @media (max-width: 768px) {
+    width: 70vw;    // Increase the width on smaller screens
+  }
+
+  // Further adjustments can be made for different screen sizes
+`;
+
+const LogoWrapper = styled.div`
+  // ... your styles for logo wrapper ...
+`;
 
 function Home() {
-  const TristateLogo = 'tri-state-coach-logo.png'
   return (
     <StyledNav className="wrapper">
-
       <SpecialMessageText className="special_message_text">
 Now Serving All of New York, New Jersey, and Connecticut</SpecialMessageText>
       <div className='hero-image-div'>
-        <img src='/tri-state-coach-logo.png' alt='tri-state-coach-logo'/>
+      <LogoWrapper>
+      </LogoWrapper>
       </div>
-      <div className="hero_content">
 
+      <div className="hero_content">
         <h1 className="hero_heading">
 Luxury &amp; Reliable Coach Bus Services</h1>
         <div className="hero_buttons_container">
-
           <a href="/college-shuttles" className="hero_button_buy w-button">
 COLLEGE&nbsp;BREAKS</a><a href="/quote" className="hero_button_buy secondary w-button">
 request a quote</a></div>
       </div>
+      
       <div className="newsletter_footer">
 <div className="newsletter_trigger_hide">
 <div className="subscribe_text">Subscribe</div>
 <div className="down_arrow_icon">
-</div></div>
-<div data-w-id="dc77ad21-2cac-f71a-82bc-e1093f437ef6" className="newsletter_trigger_show">
-<div className="subscribe_text">Subscribe</div><div className="up_arrow_icon">
-</div></div><div data-w-id="cabe2367-073e-b45f-a7bb-d3993a3955b4" className="newsletter_area">
+</div></div><div className="newsletter_area">
 <div className="newsletter_heading_block">
 <h1 className="newsletter_heading">Join Our Newsletter</h1>
 <div className="newsletter_subheading">Get notified about new trips</div></div>
 <div className="newsletter_submission_block">
 <div id="newslter" className="newsletter_form w-form">
-<form id="wf-form-Newsletter-Sign-Up" name="wf-form-Newsletter-Sign-Up" data-name="Newsletter Sign Up" method="get" className="newsletter_form_block" data-wf-page-id="64ee20d843330950bb63e0bc" data-wf-element-id="e9800251-0a2b-5dd8-acae-1643f78b0e38" aria-label="Newsletter Sign Up" >
+<form id="Newsletter-Sign-Up" name="Newsletter-Sign-Up" data-name="Newsletter Sign Up" method="get" className="newsletter_form_block">
 <input type="email" className="newsletter_form_input_field w-input" maxLength="256" name="email" data-name="Email" placeholder="email@email.com" id="email" required="" />
 <input type="submit" value="Sign up" data-wait="Please wait..." className="sign_up_button w-button" />
-</form><div className="newsletter_success w-form-done" tabIndex="-1" role="region" aria-label="Newsletter Sign Up success">
+</form><div className="newsletter_success w-form-done" tabIndex="-1" role="region">
 <div className="newsletter_success_message_block">
 <div className="newsletter_success_message">
 Thank you! Your email has been securely stored</div><div className="padlock_icon">
-</div></div></div><div className="newsletter_error w-form-fail" tabIndex="-1" role="region" aria-label="Newsletter Sign Up failure">
+</div></div></div><div className="newsletter_error w-form-fail" tabIndex="-1" role="region">
 <div className="newsletter_error_message">
 Oops! Something went wrong</div></div><div className="duplicatedemail_error">
 <div className="newsletter_duplicatedemail_error">
