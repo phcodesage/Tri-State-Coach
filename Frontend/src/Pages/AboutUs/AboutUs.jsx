5ce880f17b309e6905901a0ca3f6d6126845b3a5
phcodesage <rechceltoledo@gmail.com>
2024-01-05 13:50:12 +0800

    added: about-us

--- Frontend/src/Pages/AboutUs/AboutUs.jsx
+++ Frontend/src/Pages/AboutUs/AboutUs.jsx
@@ -2,7 +2,104 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="section">
+<div className="nav w-clearfix">
+<div className="menu_overlay">
+<div data-w-id="0a31589d-2c6a-068e-4473-e1383b9d3dc4" className="exit_menu">
+</div><div className="link_section_parent">
+<div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="book_a_ride_text">
+BOOK A RIDE</div></div><div className="nav_link">
+<a href="/" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Home<br /></div></a></div><div className="nav_link">
+<a href="/college-shuttles" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+College Shuttles<br /></div></a></div><div className="nav_link">
+<a href="/quote" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Get A Quote</div></a></div><div className="nav_link">
+<a href="/contact-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Contact Us</div></a></div></div><div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="about_us_text">
+ABOUT&nbsp;US</div></div><div className="nav_link">
+<a href="/about-us" aria-current="page" className="link_block_text w-inline-block w--current">
+<div className="nav_link_text">
+Our Buses<br /></div></a></div><div className="nav_link">
+<a href="/charter-work" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Chartering</div></a></div><div className="nav_link">
+<a href="/product/invoice" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Pay Invoice</div></a></div><div className="nav_link">
+<a href="/services" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Services</div></a></div><div className="nav_link secondary">
+<a href="/terms-of-service" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Terms of Service</div></a></div><div className="nav_link secondary">
+<a href="/privacy-policy" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Privacy Policy</div></a></div></div></div><div className="menu_action_icons">
+<a href="tel:+16315432500" className="link-block w-inline-block">
+<div className="menu_phone">
+</div></a><a href="#" className="link-block w-inline-block">
+<div className="menu_chat">
+</div></a><a href="mailto:regencybuses@gmail.com" className="link-block w-inline-block">
+<div className="menu_mail">
+</div></a><a href="https://goo.gl/maps/AGVfBWNNL3ZGRmVJ9" className="link-block w-inline-block">
+<div className="menu__location">
+</div></a></div></div><a href="/" className="logo_link_block w-inline-block">
+<img src="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png" loading="eager" sizes="(max-width: 479px) 100vw, (max-width: 767px) 75.90625px, 100vw" srcset="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-500.png 500w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-800.png 800w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png 920w" alt="" className="logo" />
+</a><div data-w-id="4c69839c-0bdc-e3ff-923e-6f7ee3cc9420" className="burger_menu">
+</div></div><div className="page_container w-container">
+<div id="w-node-a84ab36f-cd1b-9345-4903-5c62b4a3ca76-bb63e0fe" className="body_section">
+<h2 className="page_heading">
+About Us</h2><div className="article_scrollable_content_container">
+<h3 className="article_heading">
+Our Buses</h3><p className="article_text">
+Our luxurious 56-passenger Air Ride equipped Super Wide coach buses come with the latest technology and features to make your trip go by quick and cozy. Each coach comes standard with:</p><ul role="list" className="list">
+<li><p className="list_text">
+A/C</p></li><li><p className="list_text">
+DVD Player</p></li><li><p className="list_text">
+Stereo System</p></li><li><p className="list_text">
+Lavatory with flush restroom</p></li><li><p className="list_text">
+Charging outlets</p></li><li><p className="list_text">
+56 comfortable seats</p></li></ul><div className="horizontal_image_text_image">
+<div className="left_image_article">
+</div><div className="article_text_section_heading">
+<h3 className="sub_article_heading-copy">
+The Team</h3><p className="article_text_100">
+From our professional and friendly drivers who receive regular assessments and training to our in-house technicians who are skilled in industry best practices, every one here at Tri-State Coach rigorously complies with guidance from local and federal regulatory authorities as well as our industry associations.</p></div></div><h3 className="article_heading">
+Cleaning Policy</h3><p className="article_text">
+We thoroughly clean and disinfect before and after each use as follows:</p><ul role="list" className="list">
+<li><p className="list_text">
+Remove debris from the seats, furnishings, and floor</p></li><li><p className="list_text">
+Spot disinfect high touch points such as:</p><ul role="list" className="sublist">
+<li><p className="list_text">
+Entrance area handrails</p></li><li><p className="list_text">
+Parcel rack handles</p></li><li><p className="list_text">
+Door latch handles</p></li><li><p className="list_text">
+Window release bars</p></li><li><p className="list_text">
+Passenger seat headrests,armrests, seat belts and seat accessories</p></li><li><p className="list_text">
+All bathroom surfacesAll driver area surfaces</p></li><li><p className="list_text">
+All driver area surfaces</p></li></ul></li></ul><p className="article_text">
+We optimize the onboard air quality as follows:</p><ul role="list" className="list">
+<li><p className="list_text">
+We use filters rated MERV 7 or higher (removes respiratory droplets)</p></li><li><p className="list_text">
+We provide hand sanitizer</p></li><li><p className="list_text">
+We require face covering for our drivers and provide + encourage them for our passengers</p></li><li><p className="list_text">
+We strictly implore our passengers to not travel if sick or feeling unwell</p></li></ul><p className="article_text">
+Additionally, we actively invest in intensified cleaning and protection to help ensure a sanitary environment.</p><div className="associations_block">
+<div className="uma">
+</div><div className="accessibility">
+</div><div className="bany">
+</div></div></div></div></div></div>
   )
 }
 
