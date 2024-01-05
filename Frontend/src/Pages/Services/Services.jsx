1dcec7491eb20e6c0d869ba455efe33532d9e044
phcodesage <rechceltoledo@gmail.com>
2024-01-05 15:02:25 +0800

    added: service

--- Frontend/src/Pages/Services/Services.jsx
+++ Frontend/src/Pages/Services/Services.jsx
@@ -2,7 +2,100 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className="services_section">
+<div className="nav w-clearfix">
+<div className="menu_overlay">
+<div data-w-id="0a31589d-2c6a-068e-4473-e1383b9d3dc4" className="exit_menu">
+</div><div className="link_section_parent">
+<div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="book_a_ride_text">
+BOOK A RIDE</div></div><div className="nav_link">
+<a href="/" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Home<br /></div></a></div><div className="nav_link">
+<a href="/college-shuttles" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+College Shuttles<br /></div></a></div><div className="nav_link">
+<a href="/quote" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Get A Quote</div></a></div><div className="nav_link">
+<a href="/contact-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Contact Us</div></a></div></div><div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="about_us_text">
+ABOUT&nbsp;US</div></div><div className="nav_link">
+<a href="/about-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Our Buses<br /></div></a></div><div className="nav_link">
+<a href="/charter-work" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Chartering</div></a></div><div className="nav_link">
+<a href="/product/invoice" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Pay Invoice</div></a></div><div className="nav_link">
+<a href="/services" aria-current="page" className="link_block_text w-inline-block w--current">
+<div className="nav_link_text">
+Services</div></a></div><div className="nav_link secondary">
+<a href="/terms-of-service" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Terms of Service</div></a></div><div className="nav_link secondary">
+<a href="/privacy-policy" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Privacy Policy</div></a></div></div></div><div className="menu_action_icons">
+<a href="tel:+16315432500" className="link-block w-inline-block">
+<div className="menu_phone">
+</div></a><a href="#" className="link-block w-inline-block">
+<div className="menu_chat">
+</div></a><a href="mailto:regencybuses@gmail.com" className="link-block w-inline-block">
+<div className="menu_mail">
+</div></a><a href="https://goo.gl/maps/AGVfBWNNL3ZGRmVJ9" className="link-block w-inline-block">
+<div className="menu__location">
+</div></a></div></div><a href="/" className="logo_link_block w-inline-block">
+<img src="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png" loading="eager" sizes="(max-width: 479px) 100vw, (max-width: 767px) 75.90625px, 100vw" srcSet="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-500.png 500w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-800.png 800w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png 920w" alt="" className="logo" />
+</a><div data-w-id="4c69839c-0bdc-e3ff-923e-6f7ee3cc9420" className="burger_menu">
+</div></div><div className="page_container w-container">
+<div id="w-node-_59905cfd-ec11-7543-ddd6-bf0990066e1a-bb63e12d" className="body_section">
+<h2 className="page_heading">
+Trips You Can Book With Us:</h2><div className="w-layout-grid service_type_grid">
+<div id="w-node-bbcbf57b-781b-abe3-8b75-d934742d6cc1-bb63e12d" className="service-item">
+<div id="w-node-aed2cc86-a069-408d-aa18-3d881aa287dd-bb63e12d" className="service_text">
+Ground Transportation</div></div><div id="w-node-_04e9c6e5-cf7a-75e2-3364-a2556ee7a52c-bb63e12d" className="service-item">
+<div id="w-node-e0147bb9-bba5-2107-0e87-48fb0b0d0e90-bb63e12d" className="service_text">
+Corporate and Business Service</div></div><div id="w-node-_982e035f-b453-1d47-2bfb-225bf9c7176e-bb63e12d" className="service-item">
+<div id="w-node-_771962fa-5960-e5e7-ff11-67177d3ba229-bb63e12d" className="service_text">
+School and Church Groups</div></div><div id="w-node-_59620ea7-f8c8-4f22-a8a4-5a0e34ca7c2a-bb63e12d" className="service-item">
+<div id="w-node-a1124115-d502-e742-81fa-1c3a13faafc3-bb63e12d" className="service_text">
+College &amp; Professional Sports Teams</div></div><div id="w-node-a99ff52a-e55b-67b0-624b-bddea89c0b35-bb63e12d" className="service-item">
+<div id="w-node-_3b078606-56b0-4455-77f1-af7daf9b2774-bb63e12d" className="service_text">
+Conventions and Trade Shows</div></div><div id="w-node-_17abc38d-294c-9656-efd6-de36d49e4981-bb63e12d" className="service-item">
+<div id="w-node-db9a9999-d4ac-1fee-880a-bb60b2ac7884-bb63e12d" className="service_text">
+Airport Transportation and Shuttle</div></div><div id="w-node-e8169fb9-60ee-095c-cd5c-befd4aa3a439-bb63e12d" className="service-item">
+<div id="w-node-_07a22307-fc16-780b-c2ba-b1431aa9dca0-bb63e12d" className="service_text">
+Family Reunions Senior Trips</div></div><div id="w-node-_49be07c6-ff25-f6f5-48c6-80e81b13464d-bb63e12d" className="service-item">
+<div id="w-node-c0bec830-e4dc-73ae-083d-113b554d4887-bb63e12d" className="service_text">
+Employee Shuttle Programs</div></div><div id="w-node-f2dfbc34-2410-593c-d654-53a4e773887a-bb63e12d" className="service-item">
+<div id="w-node-_8d7ebbfe-b1f1-878d-d228-5dc5ef4b5475-bb63e12d" className="service_text">
+Local and National Tours</div></div><div id="w-node-_22d30392-fbd5-1f8d-5bb7-77e321d61a3c-bb63e12d" className="service-item">
+<div id="w-node-d5f3e423-4e05-57cf-cfb9-839056a2c279-bb63e12d" className="service_text">
+Educational Tour Service</div></div><div id="w-node-_801ae866-ea45-e420-b03c-d60cfdf766b9-bb63e12d" className="service-item">
+<div id="w-node-_98e2edb7-7fc8-9aa5-5e9b-cbe8e28566ad-bb63e12d" className="service_text">
+Local Sightseeing Service &amp; Tours</div></div><div id="w-node-f88bd3a1-dcfe-2979-52bb-f243f47126e3-bb63e12d" className="service-item">
+<div id="w-node-_16173a6f-da7a-a5c7-a02f-93978edc459f-bb63e12d" className="service_text">
+Corporate Relocation Services</div></div><div id="w-node-_2e4675fc-7f27-f2ee-f575-eda98cf17824-bb63e12d" className="service-item">
+<div id="w-node-ca76004a-ce3d-7c88-b886-8ec399115a9e-bb63e12d" className="service_text">
+Rallies and Concerts</div></div><div id="w-node-_7a072e6a-4df4-cb5b-8ad1-9e867a15e166-bb63e12d" className="service-item">
+<div id="w-node-c69face6-a2a4-3a68-2d61-cf35370d64a5-bb63e12d" className="service_text">
+Emergency Transportation Service</div></div><div id="w-node-_5f4c1e46-c522-ab00-6e53-54e0c5200e5c-bb63e12d" className="service-item">
+<div id="w-node-_5478312c-7bb4-1ba3-1a4c-95527ae4e1d1-bb63e12d" className="service_text">
+Weddings, Proms and Special Events</div></div></div></div><div className="associations_block">
+<div className="uma">
+</div><div className="accessibility">
+</div><div className="bany">
+</div></div></div></div>
   )
 }
 
