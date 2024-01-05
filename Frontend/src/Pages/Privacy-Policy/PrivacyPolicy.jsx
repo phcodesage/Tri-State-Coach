417e63cabcb3c38462e61368406a453fdee44909
phcodesage <rechceltoledo@gmail.com>
2024-01-05 15:09:36 +0800

    privacy-policy

--- Frontend/src/Pages/Privacy-Policy/PrivacyPolicy.jsx
+++ Frontend/src/Pages/Privacy-Policy/PrivacyPolicy.jsx
@@ -2,7 +2,110 @@ import React from 'react'
 
 function PrivacyPolicy() {
   return (
-    <div>PrivacyPolicy</div>
+    <div className="section">
+<div className="nav w-clearfix">
+<div className="menu_overlay">
+<div data-w-id="0a31589d-2c6a-068e-4473-e1383b9d3dc4" className="exit_menu">
+</div><div className="link_section_parent">
+<div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="book_a_ride_text">
+BOOK A RIDE</div></div><div className="nav_link">
+<a href="/" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Home<br /></div></a></div><div className="nav_link">
+<a href="/college-shuttles" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+College Shuttles<br /></div></a></div><div className="nav_link">
+<a href="/quote" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Get A Quote</div></a></div><div className="nav_link">
+<a href="/contact-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Contact Us</div></a></div></div><div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="about_us_text">
+ABOUT&nbsp;US</div></div><div className="nav_link">
+<a href="/about-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Our Buses<br /></div></a></div><div className="nav_link">
+<a href="/charter-work" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Chartering</div></a></div><div className="nav_link">
+<a href="/product/invoice" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Pay Invoice</div></a></div><div className="nav_link">
+<a href="/services" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Services</div></a></div><div className="nav_link secondary">
+<a href="/terms-of-service" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Terms of Service</div></a></div><div className="nav_link secondary">
+<a href="/privacy-policy" aria-current="page" className="link_block_text w-inline-block w--current">
+<div className="nav_link_text secondary">
+Privacy Policy</div></a></div></div></div><div className="menu_action_icons">
+<a href="tel:+16315432500" className="link-block w-inline-block">
+<div className="menu_phone">
+</div></a><a href="#" className="link-block w-inline-block">
+<div className="menu_chat">
+</div></a><a href="mailto:regencybuses@gmail.com" className="link-block w-inline-block">
+<div className="menu_mail">
+</div></a><a href="https://goo.gl/maps/AGVfBWNNL3ZGRmVJ9" className="link-block w-inline-block">
+<div className="menu__location">
+</div></a></div></div><a href="/" className="logo_link_block w-inline-block">
+<img src="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png" loading="eager" sizes="(max-width: 479px) 100vw, (max-width: 767px) 75.90625px, 100vw" srcSet="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-500.png 500w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-800.png 800w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png 920w" alt="" className="logo" />
+</a><div data-w-id="4c69839c-0bdc-e3ff-923e-6f7ee3cc9420" className="burger_menu">
+</div></div><div className="page_container w-container">
+<div id="w-node-_70f00a86-1ad5-8820-0598-2d8202d5c2ce-bb63e11f" className="body_section">
+<h2 className="page_heading terms-privacy">
+Privacy Policy for <br />REGENCY TRANSPORTATION, LTD.</h2><div className="article_scrollable_content_container">
+<p className="article_text">
+At Regency Buses, accessible from https://www.regencybuses.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Regency Buses and how we use it.<br /><br />If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.<br /><br />This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collected in Regency Buses. This policy is not applicable to any information collected offline or via channels other than this website.</p><h3 className="article_heading">
+Consent</h3><p className="article_text">
+By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p><h3 className="article_heading">
+Information we collect</h3><p className="article_text">
+The personal information that you are asked to provide, and the reasons you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.<br /><br />If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.<br /><br />When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p><h3 className="article_heading">
+How we use your information</h3><p className="article_text">
+We use the information we collect in various ways, including to:</p><ul role="list" className="list">
+<li><p className="list_text">
+Provide, operate, and maintain our website</p></li><li><p className="list_text">
+Improve, personalize, and expand our website</p></li><li><p className="list_text">
+Understand and analyze how you use our website</p></li><li><p className="list_text">
+Develop new products, services, features, and functionality</p></li><li><p className="list_text">
+Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</p></li><li><p className="list_text">
+Send you emails</p></li><li><p className="list_text">
+Find and prevent fraud</p></li></ul><h3 className="article_heading">
+Log Files</h3><p className="article_text">
+Regency Buses follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this as a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p><h3 className="article_heading">
+Cookies and Web Beacons</h3><p className="article_text">
+Like any other website, Regency Buses uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.<br /><br />For more general information on cookies, please read the Cookies article on Wikipedia.</p><h3 className="article_heading">
+Google DoubleClick DART Cookie</h3><p className="article_text">
+Google is one of the third-party vendors on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors somewhere else based upon their visit to www.regencybuses.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads</p><h3 className="article_heading">
+Advertising Partners Privacy Policies</h3><p className="article_text">
+You may consult this list to find the Privacy Policy for each of the advertising partners of Regency Buses.<br /><br />Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Regency Buses, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.<br /><br />Note that Regency Buses has no access to or control over these cookies that are used by third-party advertisers.</p><h3 className="article_heading">
+Third Party Privacy Policies</h3><p className="article_text">
+Regency Buses's Privacy Policy does not apply to other advertisers or websites. Thus, we advise you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. They may include details about their practices and instructions on how to opt-out of certain collections.<br /><br />You can also choose to disable cookies altogether through your browser settings. To do so, please consult each browsers' respective websites or resources concerning each brower’s cookie management.</p><h3 className="article_heading">
+CCPA Privacy Rights (Do Not Sell My Personal Information)</h3><p className="article_text">
+Under the CCPA, among other rights, California consumers have the right to:</p><ul role="list" className="list">
+<li><p className="list_text">
+Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p></li><li><p className="list_text">
+Request that a business delete any personal data about the consumer that a business has collected.</p></li><li><p className="list_text">
+Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p></li><li><p className="list_text">
+If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p></li></ul><h3 className="article_heading">
+GDPR Data Protection Rights</h3><p className="article_text">
+We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p><ul role="list" className="list">
+<li><p className="list_text">
+The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p></li><li><p className="list_text">
+The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p></li><li><p className="list_text">
+The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p></li><li><p className="list_text">
+The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p></li><li><p className="list_text">
+The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p></li><li><p className="list_text">
+The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p></li><li><p className="list_text">
+If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p></li></ul><h3 className="article_heading">
+Children's Information</h3><p className="article_text">
+Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.<br /><br />Regency Buses does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best to promptly remove such information from our records.</p></div></div></div></div>
   )
 }
 
