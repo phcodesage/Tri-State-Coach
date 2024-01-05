7fcc426bc2f20663173c2c49d21e32802c2538c3
phcodesage <rechceltoledo@gmail.com>
2024-01-05 13:59:43 +0800

    added: product-invoice

--- Frontend/src/Pages/Chartering/Chartering.jsx
+++ Frontend/src/Pages/Chartering/Chartering.jsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 function Chartering() {
   return (
-    <div>Chartering</div>
+    <div className="section">
+<div className="nav w-clearfix">
+<div className="menu_overlay">
+<div data-w-id="0a31589d-2c6a-068e-4473-e1383b9d3dc4" className="exit_menu">
+</div><div className="link_section_parent">
+<div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="book_a_ride_text">
+BOOK A RIDE</div></div><div className="nav_link">
+<a href="/" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Home<br /></div></a></div><div className="nav_link">
+<a href="/college-shuttles" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+College Shuttles<br /></div></a></div><div className="nav_link">
+<a href="/quote" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Get A Quote</div></a></div><div className="nav_link">
+<a href="/contact-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Contact Us</div></a></div></div><div className="link_section">
+<div className="link_section_heading">
+<div className="link_section_line">
+</div><div className="about_us_text">
+ABOUT&nbsp;US</div></div><div className="nav_link">
+<a href="/about-us" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Our Buses<br /></div></a></div><div className="nav_link">
+<a href="/charter-work" aria-current="page" className="link_block_text w-inline-block w--current">
+<div className="nav_link_text">
+Chartering</div></a></div><div className="nav_link">
+<a href="/product/invoice" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Pay Invoice</div></a></div><div className="nav_link">
+<a href="/services" className="link_block_text w-inline-block">
+<div className="nav_link_text">
+Services</div></a></div><div className="nav_link secondary">
+<a href="/terms-of-service" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Terms of Service</div></a></div><div className="nav_link secondary">
+<a href="/privacy-policy" className="link_block_text w-inline-block">
+<div className="nav_link_text secondary">
+Privacy Policy</div></a></div></div></div><div className="menu_action_icons">
+<a href="tel:+16315432500" className="link-block w-inline-block">
+<div className="menu_phone">
+</div></a><a href="#" className="link-block w-inline-block">
+<div className="menu_chat">
+</div></a><a href="mailto:regencybuses@gmail.com" className="link-block w-inline-block">
+<div className="menu_mail">
+</div></a><a href="https://goo.gl/maps/AGVfBWNNL3ZGRmVJ9" className="link-block w-inline-block">
+<div className="menu__location">
+</div></a></div></div><a href="/" className="logo_link_block w-inline-block">
+<img src="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png" loading="eager" sizes="(max-width: 479px) 100vw, (max-width: 767px) 75.90625px, 100vw" srcSet="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-500.png 500w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo-p-800.png 800w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/6567d3427c8946c25c6c29a0_Tri-statecoach_logo.png 920w" alt="" className="logo" />
+</a><div data-w-id="4c69839c-0bdc-e3ff-923e-6f7ee3cc9420" className="burger_menu">
+</div></div><div className="page_container w-container">
+<div id="w-node-_6a2613a5-9864-98ec-84eb-c1a339311ddd-bb63e0ff" className="body_section">
+<img src="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/632f46290479200c50b1d0b3_Regency_Buses_Coach_Bus_Fleet_Charter.png" loading="lazy" sizes="(max-width: 479px) 92vw, (max-width: 767px) 94vw, (max-width: 991px) 92vw, 94vw" srcSet="https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/632f46290479200c50b1d0b3_Regency_Buses_Coach_Bus_Fleet_Charter-p-500.png 500w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/632f46290479200c50b1d0b3_Regency_Buses_Coach_Bus_Fleet_Charter-p-800.png 800w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/632f46290479200c50b1d0b3_Regency_Buses_Coach_Bus_Fleet_Charter-p-1080.png 1080w, https://assets-global.website-files.com/62f2f32fd64065d8ceeaa74b/632f46290479200c50b1d0b3_Regency_Buses_Coach_Bus_Fleet_Charter.png 1440w" alt="Regency Buses Chartering Fleet for Your Organization" className="chartering_fleet" />
+<div className="chartering_content_block">
+<h3 className="cchartering_heading">
+Need a Transportation Department for your organization?</h3><div className="call_out_text">
+Look no further and get a tailored operational plan from our bid and contract service specialists now:</div><div className="button_group">
+<a href="tel:+16315432500" className="charter_buttons w-button">
+Call us now</a><a href="/product/invoice" className="charter_buttons w-button">
+Pay invoice</a></div></div></div></div></div>
   )
 }
 
