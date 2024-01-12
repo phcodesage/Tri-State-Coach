d7b45adf205fb3e3f666e502687960c615dc2770
phcodesage <rechceltoledo@gmail.com>
2024-01-12 23:56:33 +0800

    fixed: Quote UI

--- client/src/pages/GetAQuote.tsx
+++ client/src/pages/GetAQuote.tsx
@@ -191,7 +191,7 @@ export default function GetAQuote() {
             <div className="sm:col-span-2">
               <label htmlFor="pickup-location" className="block text-sm font-semibold leading-6 text-gray-900">More details</label>
               <div className="mt-2.5">
-                <input type="text" name="pickup-location" id="pickup-location" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6" placeholder='Things we should know, special needs, and other requests' />
+                <textarea name="pickup-location" rows={4} id="pickup-location" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm sm:leading-6" placeholder='Things we should know, special needs, and other requests' />
               </div>
             </div>
             <div className="flex justify-between">
