36abb6798f70eea29d2a95c4da1b4015a270adc3
phcodesage <rechceltoledo@gmail.com>
2024-01-13 00:40:02 +0800

    fixed: Navbar

--- client/src/Components/Hero.tsx
+++ client/src/Components/Hero.tsx
@@ -66,11 +66,11 @@ export default function Example() {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Tri-Statecoach</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
+                  src={logo}
+                  alt="tri-statecoach logo"
                 />
               </a>
               <button
