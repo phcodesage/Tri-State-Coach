050f29cf71318b0bf9897d98968465c4c50b4f0b
phcodesage <rechceltoledo@gmail.com>
2024-01-19 10:08:30 +0800

    added: invoice-header

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -36,6 +36,7 @@ export default function Navbar() {
     { name: 'Contact Us', href: '/contact-us', current: currentPath === '/contact-us' },
     { name: 'Our Buses', href: '/about-us', current: currentPath === '/about-us' },
     { name: 'Chartering', href: '/charter-work', current: currentPath === '/charter-work' },
+    
   ];
   
   function classNames(...classes:any) {
@@ -86,14 +87,6 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
--- client/src/pages/Invoice.tsx
+++ client/src/pages/Invoice.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Bushero from '../assets/Regency_Buses_Coach_Bus_Fleet_Charter-p-1080.png'
 
 function Invoice() {
     const [step, setStep] = useState(1);
@@ -17,6 +18,36 @@ function Invoice() {
 
     return (
         <>
+        
+        <div className="flex flex-col min-h-screen">
+        
+        <div className="flex-grow flex items-center justify-center px-4 py-6">
+          <div className="w-full max-w-4xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white text-center">Pay for Your Charter Trip Here</h2>
+            <a href="#">
+              <img className="rounded-t-lg w-full h-auto" src={Bushero} alt="" />
+            </a>
+            <div className="p-4 md:p-8 text-center">
+              <a href="#">
+                <h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Need a Transportation Department for your organization?</h5>
+              </a>
+              <p className="mb-4 font-normal text-gray-700 dark:text-gray-400">Look no further and get a tailored operational plan from our bid and contract service specialists now:</p>
+              <div className="flex space-x-4 justify-center items-center">
+                <a href="tel:+16315432500" className="inline-flex items-center px-3 py-2 text-m font-medium text-center text-white bg-rose-700 rounded-lg hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+                  Call Us Now
+                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+                </a>
+                <a href="/invoice" className="inline-flex items-center px-3 py-2 text-m font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300">
+                  Pay Invoice
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
             <ol className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
                 {/* Stepper UI */}
                 <li className={`flex md:w-full items-center text-${step === 1 ? "blue-600" : "gray-500"} dark:text-${step === 1 ? "blue-500" : "gray-400"}`}>
