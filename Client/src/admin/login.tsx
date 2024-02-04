0deb9d060850218bc60dec830be527aa30dfdf85
phcodesage <rechceltoledo@gmail.com>
2024-02-04 18:25:59 +0800

    fixed: login reoute

--- Client/src/admin/login.tsx
+++ Client/src/admin/login.tsx
@@ -68,14 +68,34 @@ function login() {
             <input type="name" id="username" {...register('username', { required: 'Username is required' })} className="mt-1 block w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white focus:outline-none" required />
             {errors.username && <span className="text-red-500 text-xs">{errors.username.message}</span>}
           </div>
-          <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-white">Password</label>
-            <input type={showPassword ? 'text' : 'password'} id="password" {...register('password', { required: 'Password is required' })} className="mt-1 block w-full px-3 py-2 bg-gray-700 border border-gray-600 rounded-md text-white focus:outline-none" required />
-            <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-white-400 cursor-pointer" onClick={togglePassword}>
-              {showPassword ? '🙈' : '👁️'}
-            </div>
-            {errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
+          <label htmlFor="password" className="block text-sm font-medium text-white">Password</label>
+          <div className="relative flex">
+          
+          <button className="absolute inset-y-0 right-0 pr-3 flex items-center text-white cursor-pointer"
+    onClick={togglePassword}>
+    
+      {showPassword ? (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+        </svg>
+      ) : (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+          <path d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+        </svg>
+      )}
+  </button>
+    <input
+    type={showPassword ? 'text' : 'password'}
+    id="password"
+    {...register('password', { required: 'Password is required' })}
+    className="mt-1 block w-full pl-3 pr-10 py-2 bg-gray-700 border border-gray-600 rounded-md text-white focus:outline-none"
+    required
+  />
+  
+  {errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
           </div>
+
           <button type="submit" className="w-full flex justify-center items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500 disabled:bg-rose-400">
             {isLoading ? (
               <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
